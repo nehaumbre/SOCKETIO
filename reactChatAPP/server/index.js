c02430c3db35c13739e1b2dee04280a0e08cc797
nehaumbre <nehaumbre28@gmail.com>
2026-04-05 19:06:15 +0530

    Checkpoint: React chat app
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ reactChatAPP/server/index.js
@@ -0,0 +1,44 @@
+// Packages
+import express from 'express';
+import createServer from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+//Configuration
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
+
+//middleware
+app.use(cors());
+
+
+//socket.io connection
+
+io.on('connection', (socket)=>{
+    console.log('New Client connected');
+
+    socket.on('message', (message) => {
+        console.log('Received message:', message);
+        io.emit('message', message);
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+})
+
+
+//run the server
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
